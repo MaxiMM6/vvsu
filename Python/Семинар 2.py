3ae203c486725f7cee789e06a70dd53075cf8a84
MaxiMM6 <zhlyakov145@gmail.com>
2024-10-15 19:53:52 +1000

    1

--- Python/Семинар 2.py
+++ Python/Семинар 2.py
@@ -4,7 +4,6 @@ fio = input("Введите Ваши ФИО: ")
 
 surname, name, patronymic = fio.split()
 
-
 print(f"Ваша фамилия: {surname}")
 print(f"Ваше имя: {name}")
 print(f"Ваше отчество: {patronymic}")
@@ -18,10 +17,12 @@ a = "1; 2; 3; 100"
 a_list = a.split("; ")
 a_int = []
 a_list = [a_int.append(int(n)) for n in a_list]
+
 print(a_int)
 
 
 a_float = [float(i) for i in a_int]
+
 print(a_float)
 
 
@@ -42,6 +43,7 @@ import math
 l = [12304, 123024, 54604, 2340]
 
 l2 = [math.log10(x) for x in l]
+
 print(l2)
 
 
@@ -51,4 +53,5 @@ print(l2)
 words = ["Speak ","to", "me ", "of", "Florence", "And ", "of", "the", "Renaissance"]
 
 words_clean = [i.strip().lower() for i in words]
+
 print(words_clean)
