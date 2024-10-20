4c3dafd69a8f6fbbd8d8fad4f5248b5ac03e1fbd
MaxiMM6 <zhlyakov145@gmail.com>
2024-10-20 22:16:09 +1000

    1

--- Python/Stepik/stepik.py
+++
@@ -1,7 +0,0 @@
-result = 1
-
-for i in range(10):
-    a = int(input())
-    if a != 0:
-        result *= a
-print(result)
---
+++ Python/py/Семинар 4.py
@@ -0,0 +1,34 @@
+#task1
+
+rept = {"python" : " питон",
+        "anaconda" : "анаконда",
+        "tortoize" : " черепаха" }
+
+
+
+
+#task2
+
+cnt = ["Andorra", "Belarus", "Denmark",
+"Kenya", "Jamaica", "Romania"]
+fh = [1.0, 6.0, 1.0, 4.0, 2.5, 2.0]
+
+
+
+
+
+
+#task3
+
+pairs = [(2, 4), (4, 6), (0, 1), (5, 2), (9, 1), (3, 8)]
+
+
+
+
+
+#task4s
+
+grades = {'Anna': 4, 'Bob': 3, 'Claire': 5, 'Dick': 2, 'Elena': 5,
+          'Fred': 5, 'George': 4, 'Kristina': 3, 'Nick': 2,
+          'Ursula': 4, 'Viktor': 5}
+
