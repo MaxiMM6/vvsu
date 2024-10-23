2c00b12dc0401699a052848cb62ad17b681fab39
MaxiMM6 <zhlyakov145@gmail.com>
2024-10-23 22:37:05 +1000

    1

--- Python/py/Семинар 4.py
+++ Python/py/Семинар 4.py
@@ -2,8 +2,15 @@
 
 rept = {"python" : " питон",
         "anaconda" : "анаконда",
-        "tortoize" : " черепаха" }
+        "tortoize" : " черепаха"}
 
+rept['snake'] = 'змея'
+rept['tortoise'] = rept['tortoize']
+rept.pop('tortoize')
+print(rept)
+
+for k, v in rept.items():
+    print(f'{v} по-английски будет {k}')
 
 
 
@@ -11,11 +18,13 @@ rept = {"python" : " питон",
 
 cnt = ["Andorra", "Belarus", "Denmark",
        "Kenya", "Jamaica", "Romania"]
-
 fh = [1.0, 6.0, 1.0, 4.0, 2.5, 2.0]
+dict = {}
 
+for i in range(len(cnt)):
+    dict[cnt[i]] = fh[i]
 
-
+print(dict)
 
 
 
@@ -23,7 +32,12 @@ fh = [1.0, 6.0, 1.0, 4.0, 2.5, 2.0]
 
 pairs = [(2, 4), (4, 6), (0, 1), (5, 2), (9, 1), (3, 8)]
 
+calc = {}
 
+for i in pairs:
+    calc[i] = i[0] * i[1]
+
+print(calc)
 
 
 
