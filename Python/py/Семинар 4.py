eb4dcb8008c898a43d6e124e42f8e13bfb018be7
MaxiMM6 <zhlyakov145@gmail.com>
2024-10-27 21:27:57 +1000

    1

--- Python/py/Семинар 4.py
+++ Python/py/Семинар 4.py
@@ -1,29 +1,29 @@
-#task1
+'''#task1
 
 rept = {"python" : " питон",
         "anaconda" : "анаконда",
-        "tortoize" : " черепаха"}
+        "tortoize" : " черепаха" }
 
-rept['snake'] = 'змея'
-rept['tortoise'] = rept['tortoize']
+rept["snake"] = "змея"
+rept["tortoise"] = rept["tortoize"]
 rept.pop('tortoize')
-print(rept)
 
 for k, v in rept.items():
-    print(f'{v} по-английски будет {k}')
+    print(f'{k.capitalize()} по-английски будет {v}.')
 
 
 
-#task2
+#task2 
 
 cnt = ["Andorra", "Belarus", "Denmark",
        "Kenya", "Jamaica", "Romania"]
 fh = [1.0, 6.0, 1.0, 4.0, 2.5, 2.0]
+
 dict = {}
 
 for i in range(len(cnt)):
     dict[cnt[i]] = fh[i]
-
+    
 print(dict)
 
 
@@ -37,7 +37,7 @@ calc = {}
 for i in pairs:
     calc[i] = i[0] * i[1]
 
-print(calc)
+print(calc)'''
 
 
 
@@ -47,3 +47,22 @@ grades = {'Anna': 4, 'Bob': 3, 'Claire': 5, 'Dick': 2, 'Elena': 5,
           'Fred': 5, 'George': 4, 'Kristina': 3, 'Nick': 2,
           'Ursula': 4, 'Viktor': 5}
 
+excel = []
+good = []
+satisf = []
+bad = []
+
+for k, v in grades.items():
+    if v == 2:
+        bad.append(k)
+    elif v == 3:
+        satisf.append(k)
+    elif v == 4:
+        good.append(k)
+    else:
+        excel.append(k)
+
+print(f'Excel = {excel}')
+print(f'Good = {good}')
+print(f'Satisf = {satisf}')
+print(f'Bad = {bad}')
