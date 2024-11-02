50a0a09763559f2a682e18dd7bf47b73f6aea53a
MaxiMM6 <zhlyakov145@gmail.com>
2024-11-02 23:29:59 +1000

    1

--- Python/py/Семинар 5.py
+++ Python/py/Семинар 5.py
@@ -1,11 +1,14 @@
 #task1
 
-def square(n):
-    return n ** 2
-
-n = int(input('Введите число: '))
-
-print(f'Квадрат числа равен: {square(n)}')
+def square(x):
+    result = x ** 2
+    return result
+  
+    print(f"Квадрат числа равен: {result}")
+     
+    print(f"Квадрат числа равен: {result}")
+    return result
+print(square(5))
 
 
 
@@ -14,11 +17,8 @@ print(f'Квадрат числа равен: {square(n)}')
 def nums(n):
     return [n - 1, n + 1]
 
-n = int(input('Введите число: '))
-result = nums(n)
+print(nums(7))
 
-print(result)
-    
 
 
 #task3
@@ -26,69 +26,80 @@ print(result)
 def str_lower(string):
     return [string.lower().split()]
 
-string = input('Введите строку: ')
-
-result = str_lower(string)
-
-print(result)
+print(str_lower('В лесу родилась ёлочка В лесу она росла'))
 
 
 
-#task4 
+#task4
 
 import math
 
-def my_log(num_list):
+def my_log(l):
+    
     log_list = []
 
-    for num in num_list:
+    for num in l:
         if float(num) > 0:
-            log_list.append(math.log(float(num)))
+            log_list.append(math.log(num))
         else:
             log_list.append('None')
-
+        
     return log_list
 
-num_list = (input('Введите список чисел: ')).split(',')
 
-result = my_log(num_list)
-print(result)
+print(my_log([1, 3, 2.5, -1, 9, 0, 2.71]))
 
 
 
-#task5
+#task5 
 
-def func():
+def func(list1, list2):
 
-    names = input('Введите имена людей: ')
-    age = input('Введите возраст: ')
-    
-    names_list = [x for x in names.split()]
-    age_list = [x for x in age.split()]
+    new_dict = {}
 
-    if len(names_list) == len(age_list):
-        dict_name_age = dict(zip(names_list, age_list))
-
-        return dict_name_age
+    if len(list1) == len(list2):
+        new_dict = dict(zip(list1, list2))
     else:
-        print('Списки имеют разную длину.')
-        dict_name_age = {}
-
-        return dict_name_age
+        print('Списки имеют разную длину')
     
-result = func()
-print(result)
+    return new_dict
+
+print(func(['Ann', 'Tim', 'Sam'], [12, 23, 17]))
+print(func(['Ann', 'Tim', 'Sam'], [12, 23, 17, 19]))
+
+
+
+#task6
+
+def factorial_(n):
+    f = 1
+
+    for i in range(2, n + 1):
+        f *= i
+    return f
+
+print(factorial_(5))
+
+def binom_prob(k, n):
+
+    C = factorial_(n) // factorial_(k) * factorial_(n - k)
+    return C
+
+print(binom_prob(2, 3))
 
 
 
 #task7
 
-def all_sort():
+def all_sort(*args):
 
-    numbers = input('Введите перечень чисел: ')
-    numbers_list = [int(x) for x in numbers.split(',')]
+    num = [int(numbers) for numbers in args]
+    
+    for i in range(len(num)):
+        for x in range(0, len(num) - i - 1):
+            if num[x] > num[x + 1]:
+                num[x], num[x + 1] = num[x + 1], num[x]
+    return num
 
-    return sorted(numbers_list)
+print(all_sort(7,6,1,3,8,0,-2))
 
-result = all_sort()
-print(result)
