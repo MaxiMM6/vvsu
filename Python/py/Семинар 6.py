bbd4cc4314d92986ca09c607455d1f27bc616258
MaxiMM6 <zhlyakov145@gmail.com>
2024-11-12 20:33:47 +1000

    1

--- Python/py/Семинар 6.py
+++ Python/py/Семинар 6.py
@@ -11,7 +11,6 @@ new_numbers = list(map(multi, numbers))
 print(new_numbers)
 
 #B
-
 numbers = [12, 24, 36, 48, 109, 187]
 result = list(map(lambda x: x * 7, numbers))
 print(result)
@@ -36,15 +35,12 @@ var = 11
 
 new_number = list(map(lambda x: x * var, my_number))
 
-odd = list(filter(lambda y: y % 2 == 0, new_number))
-even = list(filter(lambda z: z % 2 != 0, new_number))
-
-odd = list(map(lambda y: True, odd))
-even = list(map(lambda z: False, even))
+odd_result = list(map(lambda y: False, filter(lambda y: y % 2 != 0, new_number)))
+even_result = list(map(lambda z: True, filter(lambda z: z % 2 == 0, new_number)))
 
 print(new_number)
-print(odd)
-print(even)
+print(f'Odd {odd_result}')
+print(f'Even {even_result}')
 
 
 
