b1eea1604b1f6242de17c17725f7936329fe4706
MaxiMM6 <zhlyakov145@gmail.com>
2024-11-20 09:43:18 +1000

    Update Семинар 9.py

--- Python/py/Семинар 9.py
+++ Python/py/Семинар 9.py
@@ -1,5 +1,35 @@
 import pandas as pd
+import os
+import matplotlib
 
-df = pd.read_csv('Titanic.csv')
+os.chdir(r'C:\Users\zhlya\Documents\GitHub/vvsu/Python/py')
 
-print(df)
+#task1-2
+df = pd.read_csv('Titanic.csv', index_col = 0)
+
+#task3
+df = df.dropna()
+
+#task4
+print(df.info())
+
+#task5
+print(df.describe())
+
+#task6
+print(df['Age'].plot.hist(color = "red"))
+
+#task7
+
+
+#task8
+columns_list = list(df.columns)
+print(columns_list)
+
+#task9
+columns_list[1] = 'Class'
+df.columns = columns_list
+print(df.columns)
+
+#task10
+print(df[df['Sex'] == 0])
