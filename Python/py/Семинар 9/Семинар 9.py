a872caed9d02b586d8c064b4933495f6d379cf8a
MaxiMM6 <zhlyakov145@gmail.com>
2024-11-20 18:06:07 +1000

    1

--- Python/py/Семинар 9/Семинар 9.py
+++ Python/py/Семинар 9/Семинар 9.py
@@ -2,10 +2,10 @@ import pandas as pd
 import os
 import matplotlib
 
-os.chdir(r'C:\Users\zhlya\Documents\GitHub/vvsu/Python/py/')
+os.chdir(r'C:\Users\zhlya\Documents\GitHub/vvsu/Python/py/Семинар 9')
 
 #task1-2
-df = pd.read_csv('Titanic.csv', index_col = 0)
+df = pd.read_csv('Titanic.csv', index_col = 'PassengerId')
 
 #task3
 df = df.dropna()
@@ -20,7 +20,7 @@ print(df.describe())
 print(df['Age'].plot.hist(color = "red"))
 
 #task7
-
+df['Fare'].describe()
 
 #task8
 columns_list = list(df.columns)
@@ -32,4 +32,15 @@ df.columns = columns_list
 print(df.columns)
 
 #task10
-print(df[df['Sex'] == 0])
+print(df[df['Sex'] == 'female'])
+
+#task11
+Ymale = df[(df['Survived'] == 1) & (df['Sex'] == 'male') & (df['Age'] < 32)]
+
+#task12
+class_1_2 = df[(df['Class'] == 1) | (df['Class'] == 2)]
+
+#task13
+class_1_2_surv = df[(df['Survived'] == 1) & ((df['Class'] == 1) | (df['Class'] == 2))]
+
+#task14
