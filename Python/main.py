c6247ea465b0680ff2c9386be605c100fa4511bb
MaxiMM6 <zhlyakov145@gmail.com>
2024-12-22 16:07:05 +1000

    1

--- Python/main.py
+++ Python/main.py
@@ -1,25 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Определяем функцию
-def func(x):
-    return np.sin(2*x) + np.sqrt(3) * np.sin(x - np.pi)
+# Углы (phi) с шагом pi/8
+phi = np.linspace(0, 2*np.pi, 17)
 
-# Создаем массив значений x
-x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
-y = func(x)
+# Радиусы (r) по формуле
+r = 8 / (3 - np.cos(phi))
 
-# Находим точки пересечения с осью X
-zero_crossings = np.where(np.diff(np.sign(y)))[0]
+# Построение графика
+plt.figure(figsize=(6, 6))
+ax = plt.subplot(111, projection='polar')
+ax.plot(phi, r, marker='o')
 
-# Построим график
-plt.figure(figsize=(10, 5))
-plt.plot(x, y, label=r'$\sin(2x) + \sqrt{3} \sin(x - \pi)$')
-plt.axhline(0, color='black', lw=0.5, ls='--')
-plt.scatter(x[zero_crossings], y[zero_crossings], color='red', label='Пересечения с осью X')
-plt.title('График функции')
-plt.xlabel('x')
-plt.ylabel('f(x)')
-plt.legend()
-plt.grid()
+# Заголовок
+ax.set_title(r'$r(\phi) = \frac{8}{3 - \cos(\phi)}$', fontsize=14)
+
 plt.show()
