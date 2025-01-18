d9c974446d784276933ab6bb9e0239bb34af6d58
MaxiMM6 <zhlyakov145@gmail.com>
2025-01-18 21:02:20 +1000

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,18 +1,12 @@
-import numpy as np
-import matplotlib.pyplot as plt
+a = [a = [c]
+b = [2, 7, 8, 9, 11, 76, 25, 44]]
+b = [2, 7, 8, 9, 11, 76, 25, 44]
 
-# Углы (phi) с шагом pi/8
-phi = np.linspace(0, 2*np.pi, 17)
-
-# Радиусы (r) по формуле
-r = 8 / (3 - np.cos(phi))
-
-# Построение графика
-plt.figure(figsize=(6, 6))
-ax = plt.subplot(111, projection='polar')
-ax.plot(phi, r, marker='o')
-
-# Заголовок
-ax.set_title(r'$r(\phi) = \frac{8}{3 - \cos(\phi)}$', fontsize=14)
-
-plt.show()
+0
+1
+9
+eenvelop[EnvironmentError
+            ]
+merged a = [1, 0, 9, 12, 8, 34, 89, 91, 33, 127]
+b = [2, 7, 8, 9, 11, 76, 25, 44]
+c = [1, 0, 9, 12, 8, 34, 89, 91, 33, 100]
